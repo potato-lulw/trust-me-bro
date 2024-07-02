0341d1ee96647d9afbe88f66b1e5672494ca0b84
potato-lulw <om.patil17072@gmail.com>
2024-07-03 02:44:53 +0530

    tidied up a url

--- components/PostFeed.jsx
+++ components/PostFeed.jsx
@@ -9,12 +9,12 @@ const PostFeed = () => {
   useEffect(() => {
     const getPosts = async () => {
       try {
-        const response = await fetch('http://localhost:3000/api/posts');
+        const response = await fetch('/api/posts');
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        setPosts(data); // Assuming your API returns an array of posts
+        setPosts(data);
       } catch (error) {
         console.error('Error fetching posts:', error);
       }
